15ee2b1091c69f2294004be62200ac9a2a6df22f
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-31 01:54:29 +0900

    Add Create Component
    - 등록 폼 컴포넌트 생성
    - 등록 버튼 클릭에 따른 라우터 연동

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
           <HeaderComponent />
             <div className="container">
               <Switch> 
-                <Route path = "/" component={ListEmployeeComponent}></Route>
+                <Route path = "/" exact component={ListEmployeeComponent}></Route>
                 <Route path = "/employees" component={ListEmployeeComponent}></Route>
+                <Route path = "/add-employee" component={CreateEmployeeComponent}></Route>
                 <ListEmployeeComponent />
               </Switch>
             </div>
---
+++ client/src/components/CreateEmployeeComponent.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class CreateEmployeeComponent extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1> Employee Form</h1>
+            </div>
+        )
+    }
+}
