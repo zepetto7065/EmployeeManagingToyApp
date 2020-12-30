4c212cd525080d373a7be5b9a72d3bb73e80c9a6
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-31 01:06:32 +0900

    Add Header And Footer
    - 헤더 , 푸터 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent/> 
     </div>
   );
 }
---
+++ client/src/components/HeaderComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class HeaderComponent extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="http://zepetto7065.cafe24.com" target="_blank" className="navbar-brand">employee Management App</a></div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent;
