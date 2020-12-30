fffe81d45281f22148be471dda8b0127b950d324
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-31 02:26:31 +0900

    Add Form Handling
    - 등록 폼 핸들링 설정

--- client/src/components/CreateEmployeeComponent.jsx
+++ client/src/components/CreateEmployeeComponent.jsx
@@ -5,14 +5,64 @@ export default class CreateEmployeeComponent extends Component {
         super(props)
 
         this.state = {
-
+            firstName  : '',
+            lastName : '',
+            emailId : ''
         }
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.saveEmployee = this.saveEmployee.bind(this);
     }
-    
+    saveEmployee = (e) => {  
+        e.preventDefault();
+        let employee = {firstName : this.state.firstName, lastName : this.state.lastName, emailId: this.state.emailId};
+        console.log('employee => '+ JSON.stringify(employee));
+    }    
+    changeFirstNameHandler =(event) => {
+        this.setState({firstName:event.target.value}); 
+    }
+    changeLastNameHandler =(event) => {
+        this.setState({lastName:event.target.value}); 
+    }
+    changeEmailHandler =(event) => {
+        this.setState({emailId:event.target.value}); 
+    }
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
     render() {
         return (
             <div>
-                <h1> Employee Form</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-5 offset-md-3 offset-md-3">
+                        <h3 className="text-center">Add Employee</h3>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <label> First Name : </label>
+                                    <input placeholder="First Name" name="firstName" className="form-control" 
+                                    value={this.state.firstName} onChange={this.changeFirstNameHandler} />
+                                </div>
+                                <div className="form-group">
+                                    <label> Last Name : </label>
+                                    <input placeholder="Last Name" name="lastName" className="form-control" 
+                                    value={this.state.lastName} onChange={this.changeLastNameHandler} />
+                                </div>
+                                <div className="form-group">
+                                    <label> Email Id : </label>
+                                    <input placeholder="email Address" name="emailId" className="form-control" 
+                                    value={this.state.emailId} onChange={this.changeEmailHandler} />
+                                </div>
+
+                                <button className="btn btn-success" onClick={this.saveEmployee}>Save</button>
+                                <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft : "10px"}}>Cancel</button>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
