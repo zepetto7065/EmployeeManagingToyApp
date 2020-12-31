01e19db5061ce7ef4cf18b3ac2489497806b02f6
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-31 16:58:11 +0900

    Creating Get Employee By Id REST API
    - 상세보기 Rest API 생성

--- server/src/main/java/com/user/admin/controller/EmployeeController.java
+++ server/src/main/java/com/user/admin/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.user.admin.controller;
 
+import com.user.admin.exception.ResourceNotFoundException;
 import com.user.admin.model.Employee;
 import com.user.admin.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,4 +28,11 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+    //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable  Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : "+ id));
+        return ResponseEntity.ok(employee);
+    }
 }
